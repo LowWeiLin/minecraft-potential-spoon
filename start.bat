2251a99c7bc96150ca05bf60ee7bbad45d00a240
Low WeiLin <lwl1991@gmail.com>
2016-10-16 17:06:20 +0800

    Fix windows script

--- start.bat
+++ start.bat
@@ -1,1 +1,1 @@
-node_modules/flow-bin/flow-win64-v0.33.0/flow.exe & node node_modules/flow-remove-types/flow-node %*
+node_modules\flow-bin\flow-win64-v0.33.0\flow.exe && node node_modules\flow-remove-types\flow-node %*
